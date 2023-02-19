1ed714f4e5d04eeac5de4f814f1f55f242406485
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-02-19 12:34:20 +0600

    update class name

--- js/project-common-js.js
+++ js/project-common-js.js
@@ -1,12 +1,12 @@
 function _cmnRemoveAllErrorMessage()
 {
-    var allErrorBorder = document.getElementsByClassName('error-border');
-	var allErrorMessage = document.getElementsByClassName('error-message');
+    var allErrorBorder = document.getElementsByClassName('tool-error-border');
+	var allErrorMessage = document.getElementsByClassName('tool-error-message');
 	var i;
     // remove border
     for(i = (allErrorBorder.length) - 1; i>=0; i--)
     {
-        allErrorBorder[i].classList.remove("error-border");
+        allErrorBorder[i].classList.remove("tool-error-border");
     }
     // remove error message
     for(i = (allErrorMessage.length) - 1; i>=0; i--)
@@ -18,12 +18,12 @@ function _cmnRemoveAllErrorMessage()
 function _cmnShowErrorMessageBottomOfTheInputFiled(fieldID,errorMessage)
 {
     var inputField = document.getElementById(fieldID);   
-    inputField.classList.add("error-border"); // add border
+    inputField.classList.add("tool-error-border"); // add border
     inputField.focus(); // focus error feild
     
     var errorMessageElement = document.createElement("p"); // create a p tag for error message
     errorMessageElement.innerHTML = errorMessage; // set the error message in the p tag
-    errorMessageElement.classList.add("error-message"); // add the error message stye clsss
+    errorMessageElement.classList.add("tool-error-message"); // add the error message stye clsss
     inputField.parentNode.insertBefore(errorMessageElement, inputField.nextSibling); // set the error message uder the error feild
 }
 
